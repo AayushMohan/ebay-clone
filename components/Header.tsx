9d74f28694dbbbb2186c46fe4df9a649dec29eb5
AayushMohan <aayushmohan1702@gmail.com>
2022-11-08 16:34:47 +0530

    Added: Metamask Authentication

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
 
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  const connectWithMetamask = useMetamask();
+  const disconnect = useDisconnect();
+  const address = useAddress();
+
+  return (
+    <div>
+      <nav>
+        <div>
+          {address ? (
+            <button onClick={disconnect} className="connectWalletBtn">
+              Hi, {address}
+            </button>
+          ) : (
+            <button onClick={connectWithMetamask} className="connectWalletBtn">
+              Connect your wallet
+            </button>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;

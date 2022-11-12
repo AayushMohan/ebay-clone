5df759f602c724f31792e608db286bc64452deeb
AayushMohan <aayushmohan1702@gmail.com>
2022-11-12 23:08:58 +0530

    Added: useAddress and useContract

--- pages/addItems.tsx
+++ pages/addItems.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import Header from "../components/Header";
+import { useAddress, useContract } from "@thirdweb-dev/react";
 
 type Props = {};
 
 const addItems = (props: Props) => {
-  return <div>addItems</div>;
+  const address = useAddress();
+  const { contract } = useContract(
+    process.env.NEXT_PUBLIC_COLLECTION_CONTRACT,
+    "nft-collection"
+  );
+  console.log(contract);
+
+  return (
+    <div>
+      <Header />
+
+      <main>Add an Item to the marketplace</main>
+    </div>
+  );
 };
 
 export default addItems;

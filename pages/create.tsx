0ff008b9a5410696d2dea275f4434fa084fccb05
AayushMohan <aayushmohan1702@gmail.com>
2022-11-16 23:43:14 +0530

    Added: Styling in Main Content

--- pages/create.tsx
+++ pages/create.tsx
@@ -25,10 +25,12 @@ const Create = (props: Props) => {
       <Header />
 
       <main className="max-w-6xl mx-auto p-10 pt-2">
-        <h1>List an Item</h1>
-        <h2>Select an Item you would like to Sell!</h2>
+        <h1 className="text-4xl font-bold">List an Item</h1>
+        <h2 className="text-xl font-semibold pt-5">
+          Select an Item you would like to Sell!
+        </h2>
 
-        <hr />
+        <hr className="mb-5" />
 
         <p>Below you will find the NFT's you own in your wallet</p>
       </main>

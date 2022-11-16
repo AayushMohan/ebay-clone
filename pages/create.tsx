b8c049a450ee133e3bcaaebe1755d890474c6411
AayushMohan <aayushmohan1702@gmail.com>
2022-11-16 23:41:53 +0530

    Added: Main Content

--- pages/create.tsx
+++ pages/create.tsx
@@ -23,6 +23,15 @@ const Create = (props: Props) => {
   return (
     <div>
       <Header />
+
+      <main className="max-w-6xl mx-auto p-10 pt-2">
+        <h1>List an Item</h1>
+        <h2>Select an Item you would like to Sell!</h2>
+
+        <hr />
+
+        <p>Below you will find the NFT's you own in your wallet</p>
+      </main>
     </div>
   );
 };

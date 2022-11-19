ca8c536f1cc7365c6bf1f02104da25ab17176558
AayushMohan <aayushmohan1702@gmail.com>
2022-11-19 18:45:13 +0530

    Added: Header component in listing page

--- pages/listing/[listing].tsx
+++ pages/listing/[listing].tsx
@@ -1,12 +1,17 @@
 import { useRouter } from "next/router";
 import React from "react";
+import Header from "../../components/Header";
 
 const ListingPage = () => {
   const router = useRouter();
   const { listingId } = router.query as { listingId: string };
 
   console.log(listingId);
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default ListingPage;
